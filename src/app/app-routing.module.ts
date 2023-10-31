63197ce29fcfc9d6782babfa498d372f8dda4f3c
Rafael Soares <sawarafael@gmail.com>
2023-10-31 19:13:56 -0300

    feat: first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { TarefasComponent } from './pages/tarefas/tarefas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'tarefas',
+    component: TarefasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { DatatableComponent } from './components/datatable/datatable.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TarefasComponent } from './pages/tarefas/tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefasComponent,
+    LoginComponent,
+    DatatableComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
